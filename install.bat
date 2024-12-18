8d60339354ae27fe04ead479a07192e3e9933ab8
Mohamed Helmy <MohamedLegendary2005@gmail.com>
2024-12-18 18:57:15 +0200

    added install.bat and run.bat to make sure the app is running as admin

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Store the original path of the script
+set "script_path=%~dp0"
+
+:: Check for admin privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Add your admin tasks below
+cd /d %script_path%
+pip install -r requirements.txt
+pause
